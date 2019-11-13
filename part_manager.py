dbea4b1809130d95e09085ff2b5d17521aee5df2
omokehinde <engr.omokehinde@gmail.com>
2019-11-13 23:29:59 +0100

    Delete Item

--- part_manager.py
+++ part_manager.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 from db import Database
 
 db = Database('store.db')
@@ -9,19 +10,46 @@ def populate_list():
         parts_list.insert(END, row)
 
 def add_item():
+    if part_text.get()=='' or customer_text.get()=='' or retailer_text.get()=='' or price_text.get()=='':
+        messagebox.showerror('Required Fields', 'Please include all fields')
+        return
     db.insert(part_text.get(), customer_text.get(), retailer_text.get(), price_text.get())
     parts_list.delete(0, END)
     parts_list.insert(END, (part_text.get(), customer_text.get(), retailer_text.get(), price_text.get()))
+    
+    clear_text()
     populate_list()
 
+def select_item(event):
+    global selected_item
+    index = parts_list.curselection()[0]
+    selected_item = parts_list.get(index)
+
+    part_entry.delete(0, END)
+    part_entry.insert(END, selected_item[1])
+
+    customer_entry.delete(0, END)
+    customer_entry.insert(END, selected_item[2])
+
+    retailer_entry.delete(0, END)
+    retailer_entry.insert(END, selected_item[3])
+
+    price_entry.delete(0, END)
+    price_entry.insert(END, selected_item[4])
+
 def remove_item():
-    print('Remove')
+    db.remove(selected_item[0])
+    clear_text()
+    populate_list()
 
 def update_item():
     print('Update')
 
 def clear_text():
-    print('Clear')
+    part_entry.delete(0, END)
+    customer_entry.delete(0, END)
+    retailer_entry.delete(0, END)
+    price_entry.delete(0, END)
 
 # create a window object 
 app = Tk()
@@ -59,6 +87,8 @@ scrollbar.grid(row=3, column=3)
 # set scroll to list box
 parts_list.configure(yscrollcommand=scrollbar.set)
 scrollbar.configure(command=parts_list.yview)
+# Bind Select
+parts_list.bind('<<ListboxSelect>>', select_item)
 # buttons
 add_btn = Button(app, text='Add Part', width=12, command=add_item)
 add_btn.grid(row=2, column=0, pady=20)
