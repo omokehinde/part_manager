9e29adb3a4713853e41c4a643f03efb50bc09314
omokehinde <engr.omokehinde@gmail.com>
2019-11-13 02:40:38 +0100

    Initial commit

---
+++ part_manager.py
@@ -0,0 +1,44 @@
+from tkinter import *
+
+# create a window object 
+app = Tk()
+
+# part 
+part_text = StringVar()
+part_label = Label(app, text='Part Name', font=('bold', 14), pady=20)
+part_label.grid(row=0, column=0, sticky=W)
+part_entry = Entry(app, textvariable=part_text)
+part_entry.grid(row=0, column=1)
+# Customer 
+customer_text = StringVar()
+customer_label = Label(app, text='Customer', font=('bold', 14))
+customer_label.grid(row=0, column=2, sticky=W)
+customer_entry = Entry(app, textvariable=customer_text)
+customer_entry.grid(row=0, column=3)
+# Retailer 
+retailer_text = StringVar()
+retailer_label = Label(app, text='Retailer', font=('bold', 14))
+retailer_label.grid(row=1, column=0, sticky=W)
+retailer_entry = Entry(app, textvariable=retailer_text)
+retailer_entry.grid(row=1, column=1)
+# Price
+price_text = StringVar()
+price_label = Label(app, text='Price', font=('bold', 14))
+price_label.grid(row=1, column=2, sticky=W)
+price_entry = Entry(app, textvariable=price_text)
+price_entry.grid(row=1, column=3)
+# Parts List (Listbox)
+parts_list = Listbox(app, height=8, width=70, border=0)
+parts_list.grid(row=3, column=0, columnspan=3, rowspan=6, pady=20, padx=20)
+# Create Scrollbar
+scrollbar = Scrollbar(app)
+scrollbar.grid(row=3, column=3)
+# set scroll to list box
+parts_list.configure(yscrollcommand=scrollbar.set)
+scrollbar.configure(command=parts_list.yview)
+
+app.title('Part Manager')
+app.geometry('650x325')
+
+# start program 
+app.mainloop()
